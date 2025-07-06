20e46df7c8532bfaa9e2033b1eecde78f6c7ca99
SwadheenMishra <144024958+SwadheenMishra@users.noreply.github.com>
2025-07-06 11:22:47 +0530

    Update InstaHelper.py

--- software/App/InstaHelper.py
+++ software/App/InstaHelper.py
@@ -2,7 +2,7 @@ from instagrapi import Client
 
 def upload(path: str, caption: str = "get gud? lol") -> None:
     cl = Client()
-    cl.login("gymdudetest", "thisisacocfollowingpassword")
+    cl.login("YOUR ID", "YOUR PASSWORD")
 
     cl.photo_upload(path, caption)
 
